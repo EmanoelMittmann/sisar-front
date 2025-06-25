485eefbf2fad313c4c4e65ff66be43246259cc4a
EmanoelMittmann <emanoelmittmann@hotmail.com>
2025-06-24 23:52:14 -0300

    styles: logo-navigation-wrong

--- src/app/agendamento-publico/layout.tsx
+++ src/app/agendamento-publico/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <nav className="h-16 bg-[#9E9E9E] w-full flex items-center">
+      <nav className="h-16 bg-[#9E9E9E] px-12 w-full flex items-center">
         <h5 className="text-4xl text-[#fff]">sisar</h5>
       </nav>
       <div className="h-full w-full">{children}</div>
